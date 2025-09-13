5a75779b044f870982f48701724b0d83da3286d4
bobby3085 <bobby3085@gmail.com>
2025-09-13 05:37:58 +0000

    change

--- ec2instances.sh
+++ ec2instances.sh
@@ -1,11 +1,19 @@
 #!/bin/bash
 #Get list of all AWS regions
+<<<<<<< HEAD
+=======
+#.exe not required in aws.exe
+>>>>>>> 11da65f (change)
 regions=$(aws.exe ec2 describe-regions --output text --query 'Regions[].RegionName')
 
 #loop through all regions
 for region in $regions; do
 	echo "instances in region ; $region"
 	#describe instances in the current region
+<<<<<<< HEAD
+=======
+	#remove .exe from aws.exe 
+>>>>>>> 11da65f (change)
 	aws.exe ec2 describe-instances --region "$region"\
 		--query 'Reservations[*].Instances[*].[InstanceId, State.Name, InstanceType, Placement.AvailabiltyZone, Tags[?Key=='Name'].value |[0]]'\
 		--output table
